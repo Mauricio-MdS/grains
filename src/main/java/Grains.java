9f029d2692a8011b331c2bdcd421b1a32926517f
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-03-25 17:44:19 -0300

    Exercise solution

--- src/main/java/Grains.java
+++ src/main/java/Grains.java
@@ -3,11 +3,16 @@ import java.math.BigInteger;
 class Grains {
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (square < 1 || square > 64) throw new IllegalArgumentException("square must be between 1 and 64");
+
+        return BigInteger.valueOf(2).pow(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        var sum = BigInteger.ZERO;
+        for (int i = 1; i <= 64; i++) sum = sum.add(grainsOnSquare(i));
+
+        return sum;
     }
 
 }
